aef83fcab5678978a6cf122c2ed00af703edc1f6
lamesjinden <jamesmlinden@gmail.com>
2014-04-15 01:29:28 -0400

    Initial Commit: Library + MSBuild Task
    First commit. Including core library and MSBuild task wrapper.

---
+++ Flatten.Core/Flattener.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Flatten.Core
+{
+
+    /// <summary>
+    /// Flattens XDocument instances
+    /// </summary>
+    public static class Flattener
+    {
+
+        public static char[] DefaultDelimiters
+        {
+            get { return new[] { ';', ',', ' ' }; }
+        }
+
+        /// <summary>
+        /// Creates a new instance of XDocument where each selected element from <paramref name="xpath"/> 
+        /// is replaced by one or more copies for each token contained in the value of <paramref name="child"/>.
+        /// Tokens are delimited by ';', ',', and ' ' charactes.
+        /// </summary>
+        /// <param name="src">Source XDocument instance</param>
+        /// <param name="xpath">XPath expression to select elements containing composite values</param>
+        /// <param name="child">Name of the child elemement of elements returned by <paramref name="xpath"/>, potentially containing composite values</param>
+        /// <returns>New XDocument instance</returns>
+        public static XDocument Flatten(XDocument src, string xpath, XName child)
+        {
+            return Flatten(src, xpath, child, DefaultDelimiters);
+        }
+
+        /// <summary>
+        /// Creates a new instance of XDocument where each selected element from <paramref name="xpath"/> 
+        /// is replaced by one or more copies for each token contained in the value of <paramref name="child"/>.
+        /// </summary>
+        /// <param name="src">Source XDocument instance</param>
+        /// <param name="xpath">XPath expression to select elements containing composite values</param>
+        /// <param name="child">Name of the child elemement of elements returned by <paramref name="xpath"/>, potentially containing composite values</param>
+        /// <param name="delimiters">array of delimiting charaters</param>
+        /// <returns>New XDocument instance</returns>
+        public static XDocument Flatten(XDocument src, string xpath, XName child, char[] delimiters)
+        {
+            var local = new XDocument(src);
+            var delimitersSet = new HashSet<char>(delimiters);
+
+            foreach (var element in local.XPathSelectElements(xpath)
+                                         .Where(element => element.Element(child) != null)
+                                         .Where(element =>
+                                             delimitersSet.Any(delimiter => element.Element(child).Value.Contains(delimiter)))
+                                         .ToList())
+            {
+                var unpacked = Unpack(element, child, delimitersSet.ToArray());
+                element.AddAfterSelf(unpacked);
+                element.Remove();
+            }
+
+            return local;
+        }
+
+        private static IEnumerable<XElement> Unpack(XElement element, XName child, char[] delimiters)
+        {
+            return element.Element(child).Value
+                                         .Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+                                         .Select(value =>
+                                         {
+                                             var split = new XElement(element);
+                                             split.Element(child).Value = value;
+                                             return split;
+                                         });
+        }
+
+    }
+
+}
---
+++ Flatten.Task/FlatMap.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using Flatten.Core;
+using Microsoft.Build.Framework;
+
+namespace Flatten.Task
+{
+
+    public class FlatMap : Microsoft.Build.Utilities.Task
+    {
+
+        private const string DefaultPrefix = "flat";
+
+        [Required]
+        public ITaskItem[] XmlFiles { get; set; }
+
+        [Required]
+        public string ElementSelector { get; set; }
+
+        [Required]
+        public string KeyName { get; set; }
+
+        public string ResultPrefix { get; set; }
+
+        public string Delimiters { get; set; }
+
+        [Output]
+        public string[] Results { get; set; }
+
+        private void LogExecutionState()
+        {
+            Log.LogMessage(MessageImportance.High, "ElementSelector={0}", ElementSelector);
+            Log.LogMessage(MessageImportance.High, "KeyName={0}", KeyName);
+            if (!string.IsNullOrWhiteSpace(ResultPrefix)) Log.LogMessage(MessageImportance.High, "ResultPrefix={0}", ResultPrefix);
+            if (!string.IsNullOrWhiteSpace(Delimiters)) Log.LogMessage(MessageImportance.High, "Delimiters={0}", string.Join("", Delimiters.ToCharArray()));
+        }
+
+        public override bool Execute()
+        {
+            LogExecutionState();
+
+            List<string> results = new List<string>();
+            foreach (var item in XmlFiles)
+            {
+                Log.LogMessage("Processing {0}...", item.ItemSpec);
+
+                try
+                {
+                    XDocument flattenedDocument;
+                    using (var stream = new FileStream(item.ItemSpec, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        var xdocument = XDocument.Load(stream);
+                        var delimiters = string.IsNullOrWhiteSpace(Delimiters)
+                                             ? Flattener.DefaultDelimiters
+                                             : Delimiters.ToCharArray();
+                        flattenedDocument = Flattener.Flatten(xdocument, ElementSelector, KeyName, delimiters);
+                    }
+
+                    var directoryName = Path.GetDirectoryName(item.ItemSpec);
+                    var originalName = Path.GetFileName(item.ItemSpec);
+                    string flattenedName = string.Format("{0}.{1}",
+                                               string.IsNullOrWhiteSpace(ResultPrefix) 
+                                                   ? DefaultPrefix 
+                                                   : ResultPrefix,
+                                               originalName);
+                    var flattenedPath = Path.Combine(directoryName, flattenedName);
+                    
+                    if (File.Exists(flattenedPath)) Log.LogMessage(MessageImportance.High, "Overwriting {0}", flattenedPath);
+
+                    using (var stream = new FileStream(flattenedPath, FileMode.Create, FileAccess.Write, FileShare.Read))
+                    {
+                        flattenedDocument.Save(stream, SaveOptions.None);
+                    }
+
+                    results.Add(flattenedPath);
+
+                    Log.LogMessage("Processing {0} completed successfully", item.ItemSpec);
+                }
+                catch (Exception exception)
+                {
+                    Log.LogError("Processing {0} failed", item.ItemSpec);
+                    Log.LogErrorFromException(exception);
+
+                    return false;
+                }
+
+                Results = results.ToArray();
+            }
+
+            return true;                       
+        }
+
+    }
+
+}
